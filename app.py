36c1df1dbf7d4f24734379f245c2ce7a600aa2aa
schlys <samchlystek20@gmail.com>
2023-01-11 18:00:19 -0500

    listen for changes

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
-import controller, flask, requests, json
+import controller, flask, git
 
 app = Flask(__name__)
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./Spotify-Playlist-WebApp')
+        origin = repo.remotes.origin
+        repo.create_head('main', 
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
 
 
 @app.route('/', methods=['POST', 'GET'])
